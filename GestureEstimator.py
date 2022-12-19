fecfd4ba48929d4d600f3244f0681fc3b8b82819
Moey Misrabi <momisrabi@gmail.com>
2022-12-19 12:39:58 +0300

    GestureEstimator class init

---
+++ GestureEstimator.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+
+class gestur_estimator:
+    
+    def __init__(self):
+        None
+    
+    def estimate_from_tips(tips):
+        None
+    
+    def estimate_from_area(area):
+        None
